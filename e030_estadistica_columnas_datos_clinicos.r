0aafe600cb5dfa0a7a8b553418738ce082bacbd0
JulenMendieta <julenmendieta92@gmail.com>
2015-05-26 11:21:52 +0200

    Versión final

--- e030_estadistica_columnas_datos_clinicos.r
+++ e030_estadistica_columnas_datos_clinicos.r
@@ -5,18 +5,26 @@ date ()
 Sys.info ()[c("nodename", "user")]
 commandArgs ()
 rm (list = ls ())
-R.version.string ##"R version 3.1.2 (2014-10-31)"
+R.version.string ##"R version 3.2.0 (2015-04-16)"
 
-setwd ("/home/jmendieta/Documents/revision_mirnas/datos/procesados")
-datosClinicos <- readRDS("e010_tabla_datos_clinicos.RData")
+try (source (".job.r")); try (.job)
+
+options (width = 170)
+#options (width = 1000)
+
+setwd (file.path (.job$dir$proces))
+
+load (file.path (.job$dir$proces, "clinical_info_all.RData"))
 lnombres <- list()
 for (id in names (datosClinicos)) {
   datos0 <- datosClinicos[[id]]
+ 
+  #Quitamos los datos en los que todo sea NA
   todo.na <- apply (is.na (datos0), 2, all)
   table (todo.na)
-  datos0 <- datos0[,!todo.na]  #Quitamos los datos en los que todo sea NA
-     
-  lon <- apply (datos0, 2, function (x) length (setdiff (unique (x), NA)))  #Guardamos en lon cuantos datos diferentes hay en cada cabecera
+  datos0 <- datos0[,!todo.na]  
+  #Guardamos en lon cuantos datos diferentes hay en cada cabecera   
+  lon <- apply (datos0, 2, function (x) length (setdiff (unique (x), NA)))  
   table (lon)
   nombres <- colnames(datos0)   #Con esto miramos todas las cabeceras que han permanecido hasta ahora
   lnombres[[id]] <- nombres     #Guardamos las cabeceras que hay asociadas al nombre del fichero en lnombres
@@ -36,4 +44,15 @@ mat[1:3,]
 dim (mat)
 table (rowSums (mat))  #Esto nos muestra cuantas cabeceras hay solo en x ficheros (cuantas en 1, cuantas en 2, cuantas en 4 etc.)
 ###De aqui guardamos la matriz mat, ya que de ella se extrae cuantas columnas hay en cada fichero
-saveRDS(mat, file="e030_estadistica_columnas_datos_clinicos.RData")
+save (list = "mat", file = file.path (.job$dir$proces, "estadistica_columnas_datos_clinicos.RData"))
+
+# colnames (mat) <- NULL
+# touse <- rowSums (mat) == length(datosClinicos) #Guardamos solo las cabeceras que esten en todos los ficheros
+# touse
+# mat[touse,]
+# rownames (mat)[touse]  #Muestra los nombres de las cabeceras q estan en todos los ficheros
+
+###EXIT
+warnings ()
+sessionInfo ()
+q ("no")
--- e040_manejo_datos_clinicos.r
+++ e040_manejo_datos_clinicos.r
@@ -1,24 +1,78 @@
 ## e040_manejo_datos_clinicos.r
 ## 2015-03-26 julenmendieta92@gmail.com
-## Script para manjear los datos clinicos. Está sin terminar y puede que no se use
+## Script para manejar los datos clinicos y ver los diferentes datos clinicos que hay y en cuantos de los cánceres.
 date ()
 Sys.info ()[c("nodename", "user")]
 commandArgs ()
 rm (list = ls ())
-R.version.string ##"R version 3.1.2 (2014-10-31)"
+R.version.string ##"R version 3.2.0 (2015-04-16)"
 
-setwd ("/home/jmendieta/Documents/revision_mirnas/datos/procesados")
-datosClinicos <- readRDS("e010_tabla_datos_clinicos.RData")
+try (source (".job.r")); try (.job)
+
+options (width = 170)
+#options (width = 1000)
+
+setwd (file.path (.job$dir$raw, "clinical"))
+ficheros <- dir (pattern = "clinical_patient", recursive= TRUE)  #Guardamos en ficheros el stream de cada carpeta y los ficheros que contiene
+
+setwd (file.path (.job$dir$proces))
+
+load (file.path (.job$dir$proces, "clinical_info_all.RData"))
 summary(datosClinicos)
 
-infoDatosClinicos <- readRDS("e020_revision_datos_clinicos.RData")
-infoDatosClinicos
+load (file.path (.job$dir$proces, "estadistica_columnas_datos_clinicos.RData"))
+presenciaColumnas <- mat
+mat <- NULL
 
-
-presenciaColumnas <- readRDS("e030_estadistica_columnas_datos_clinicos.RData")
 table (rowSums (presenciaColumnas))  #Esto nos muestra cuantas cabeceras hay solo en x ficheros (cuantas en 1, cuantas en 2, cuantas en 4 etc.)
 colnames (presenciaColumnas) <- NULL
 touse <- rowSums (presenciaColumnas) == length(datosClinicos) #Guardamos solo las cabeceras que esten en todos los ficheros
 touse
 presenciaColumnas[touse,]
 rownames (presenciaColumnas)[touse]  #Muestra los nombres de las cabeceras q estan en todos los ficheros
+
+
+
+# ###PROBLEMA: ¿Pregunto al usuario mejor cual quiere que sea el mínimo de columnas?
+# tamanyofich <- length(ficheros)
+# ##Voy a poner un bucle para incluir datos hasta tener un numero aceptable (digamos 10):
+# while(table(touse)[[2]] < 10) {   #Mientras que el numero de TRUE en touse sea menor que 10
+#   touse[rowSums (mat) == tamanyofich - 1] = TRUE
+#   tamanyofich <- tamanyofich - 1
+# }  
+# ##
+# 
+# touse
+# mat[touse,]
+# rownames (mat)[touse]  #Muestra los nombres de las 10 cabeceras mas frecuentes
+
+setwd (file.path (.job$dir$raw, "clinical"))
+
+#Con esto guardamos las columnas que son comunes a todos los ficheros
+
+tablist <- list ()
+for (fi in ficheros) {
+  nombres <- unlist (strsplit (readLines (fi)[1], split = "\t"))
+  datos0 <- read.table (fi, header = TRUE, sep = "\t", quote = "", col.names = nombres, as.is = TRUE, na.strings = c("[Not Available]", "[Not Applicable]", "[Not Evaluated]", "[Unknown]", "NA"), skip = 3)
+  #Con esto miramos cuantas columnas de las presentes en todos los datos tenemos hasta ahora
+  table (colnames(datos0) %in% rownames (presenciaColumnas)[touse])
+  mantener <- colnames(datos0) %in% rownames (presenciaColumnas)[touse]
+  #Y las guardamos
+  datos0 <- datos0[mantener]
+  ##Hasta aqui
+  
+  #Guardamos el tag
+  tag <- sapply (strsplit (sapply (strsplit (fi, split = "_"), function (x) x[2]), split = "\\."), function (x) x[1])
+  
+  print (dim (datos0))
+  tablist[[tag]] <- datos0
+  
+  #Con este bucle guardamos en una lista todos los datos que hay en cada fichero
+}
+
+save (list = "tablist", file = file.path (.job$dir$proces, "datos_clinicos_comunes.RData"))
+
+###EXIT
+warnings ()
+sessionInfo ()
+q ("no")
